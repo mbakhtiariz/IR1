2533451d8150bde174de78eea2176cc34972c17d
mdhk <mdhk@users.noreply.github.com>
2018-01-27 15:02:57 +0100

    results plots for the language models with different parameter settings

--- HW2/get_results.sh
+++ HW2/get_results.sh
@@ -1,23 +1,24 @@
-echo -n > results.txt
+qrel_set='validation'
+echo -n > results_$qrel_set.txt
 for f in run_files/*.run
 do
   model=$(echo "$f" | grep -oE '/[^/]+\.' | cut -c2- | rev | cut -c2- | rev)
 
-  echo "MODEL: $model" >> results.txt
+  echo "MODEL: $model" >> results_$qrel_set.txt
 
   # NDCG@10
-  out=$(./trec_eval/trec_eval -m ndcg_cut.10 -q ap_88_89/qrel_validation $f)
-  echo "$out" >> results.txt
+  out=$(./trec_eval/trec_eval -m ndcg_cut.10 -q ap_88_89/qrel_$qrel_set $f)
+  echo "$out" >> results_$qrel_set.txt
 
   # MAP@1000
-  out=$(./trec_eval/trec_eval -m map_cut.1000 -q ap_88_89/qrel_validation $f)
-  echo "$out" >> results.txt
+  out=$(./trec_eval/trec_eval -m map_cut.1000 -q ap_88_89/qrel_$qrel_set $f)
+  echo "$out" >> results_$qrel_set.txt
 
   # Precision@5
-  out=$(./trec_eval/trec_eval -m P.5 -q ap_88_89/qrel_validation $f)
-  echo "$out" >> results.txt
+  out=$(./trec_eval/trec_eval -m P.5 -q ap_88_89/qrel_$qrel_set $f)
+  echo "$out" >> results_$qrel_set.txt
 
   # Recall@1000
-  out=$(./trec_eval/trec_eval -m recall.1000 -q ap_88_89/qrel_validation $f)
-  echo "$out" >> results.txt
+  out=$(./trec_eval/trec_eval -m recall.1000 -q ap_88_89/qrel_$qrel_set $f)
+  echo "$out" >> results_$qrel_set.txt
 done
